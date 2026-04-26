fa59447127ff2f3cf0586491d712c22d0b5b0dd9
joyadas092 <153970810+joyadas092@users.noreply.github.com>
2026-04-26 08:55:42 +0530

    Update doomdivider.py

--- doomdivider.py
+++ doomdivider.py
@@ -245,7 +245,7 @@ async def send(id, message,processed):
 
     Promo = InlineKeyboardMarkup(
         [[
-          InlineKeyboardButton("💬 Follow WhatsApp", url="https://whatsapp.com/channel/0029Va9OziWGE56p30A1de0Q")]]
+          InlineKeyboardButton("💬 All LootDeals", url="https://t.me/Deal_Doom/1721")]]
     )
     notify = should_notify(id)   # ✅ Added line
 
@@ -254,10 +254,14 @@ async def send(id, message,processed):
             modifiedtxt = compilehyperlink(message).replace('@under_99_loot_deals', '@shopsy_meesho_Deals')
             if 'amazon' in modifiedtxt:
                 # print('amzn working')
+                urls = extract_link_from_text2(modifiedtxt)
+                Newtext = modifiedtxt
+                for url in urls:
+                    Newtext = Newtext.replace(url, f'<b><a href={url}>Buy Now</a></b>')
                 await app.send_photo(chat_id=id,
                                      # photo=message.photo.file_id,
                                      photo=processed,
-                                     caption=f'<b>{ekconvert(modifiedtxt)}</b>' + "\n\n<b>👉 <a href ='https://t.me/addlist/WhyK9RPZHdU4MGNl'>For More Deals 🥳, Click & JOIN FAST🔥</a> 👈</b>",
+                                     caption=f'<b>{Newtext}</b>' + "\n\n<b>👉 <a href ='https://t.me/addlist/WhyK9RPZHdU4MGNl'>For More Deals 🥳, Click & JOIN FAST🔥</a> 👈</b>",
                                      reply_markup=Promo,
                                      disable_notification = not notify )
             else:
@@ -274,9 +278,13 @@ async def send(id, message,processed):
 
     elif message.text:
         modifiedtxt = compilehyperlink(message).replace('@under_99_loot_deals', '@shopsy_meesho_Deals')
-        if 'tinyurl' in modifiedtxt or 'amazon' in modifiedtxt:
+        if 'amazon' in modifiedtxt:
+                urls = extract_link_from_text2(modifiedtxt)
+                Newtext = modifiedtxt
+                for url in urls:
+                    Newtext = Newtext.replace(url, f'<b><a href={url}>Buy Now</a></b>')
             await app.send_message(chat_id=id,
-                                   text=f'<b>{ekconvert(modifiedtxt)}</b>',
+                                   text=f'<b>{Newtext}</b>',
                                    disable_web_page_preview=True,disable_notification = not notify)
         else:
             await app.send_message(chat_id=id,
